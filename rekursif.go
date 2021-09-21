617b9e28dcdb465d403c4ce6561b13d180198e82
Aura Qolbani <aura.qolbani@gmail.com>
2021-09-21 19:16:44 +0700

    add function upperCase

--- rekursif.go
+++ rekursif.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"strings"
 )
 
 var total = 0
@@ -41,14 +42,38 @@ func deretPecahan(i int, jumlah int, pecahan string) int {
 	}
 }
 
+func upperCase(kata string, i int) int {
+	upper := strings.ToUpper(kata)
+
+	if i < len(kata) {
+		var filter bool
+		if string(kata[i]) == string(upper[i]) {
+			filter = true
+		}
+		if filter == true {
+			return 1 + upperCase(kata, i+1)
+		} else {
+			return 0 + upperCase(kata, i+1)
+		}
+	} else {
+		return 0
+	}
+}
+
 func main() {
 	var jumlah int
+	var kata string
 
 	fmt.Print("Masukan jumlah deret : ")
 	fmt.Scan(&jumlah)
+	fmt.Print("Masukan Kata : ")
+	fmt.Scan(&kata)
 
 	fmt.Print("1. ")
 	fmt.Println(" =", deretAngka(jumlah, 5))
 	fmt.Print("2. ")
 	fmt.Println("= x^ ", "/", deretPecahan(1, jumlah, "penyebut"))
+	fmt.Print("3. ")
+	fmt.Printf("Jumlah huruf kapital pada kata %s : ", kata)
+	fmt.Println(upperCase(kata, 0))
 }
